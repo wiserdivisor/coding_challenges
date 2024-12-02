9bf1927ca257de5b26109bb3b988ddbb890ae75e
wiserdivisor <dat0user@gmail.com>
2024-12-02 23:41:16 +0530

    day 1: part 1 solved

--- AOC2024/day_1/solution.cpp
+++ AOC2024/day_1/solution.cpp
@@ -5,13 +5,13 @@
 int fn_partition_i(int ia_unsorted[], int i_low, int i_high) {
   int i_pivot = ia_unsorted[i_high];
   int i = i_low - 1;
-  for (int j = low; j < high; j++) {
+  for (int j = i_low; j < i_high; j++) {
     if (ia_unsorted[j] < i_pivot) {
       i++;
-      swap(ia_unsorted[i], ia_unsorted[j]);
+      std::swap(ia_unsorted[i], ia_unsorted[j]);
     }
   }
-  swap(ia_unsorted[i+1], ia_unsorted[i_high]);
+  std::swap(ia_unsorted[i+1], ia_unsorted[i_high]);
   return i+1;
 }
 
@@ -25,14 +25,16 @@ void fn_quicksort_vo(int ia_unsorted[], int i_low, int i_high) {
 
 int main() {
   std::cout << "Day 1 : Historian Hysteria" << std::endl;
-  std::ifstream input_data("./input.txt");
-  long total = 0;
+
+  long l_total = 0;
   char ca_line[14];
   int ia_llocid[1000];
   int ia_rlocid[1000];
+
+  std::ifstream input_data("./input.txt");
   for(int i=0; i<1000; i++) {
 
-    input_data.getca_line(ca_line,sizeof(ca_line)); //12345\s\s\s12345
+    input_data.getline(ca_line,sizeof(ca_line)); //12345\s\s\s12345
 
     char *ca_llocid = ca_line;
     ca_llocid[5] = '\0';
@@ -44,9 +46,15 @@ int main() {
     ia_rlocid[i] = std::stoi(ca_rlocid);
 
   }
-  for(auto x: ia_llocid){
-    std::cout << x << std::endl;
+  input_data.close();
+
+  fn_quicksort_vo(ia_llocid,0,999);
+  fn_quicksort_vo(ia_rlocid,0,999);
+
+  for(int i=0; i<1000; i++){
+    l_total += std::abs(ia_llocid[i] - ia_rlocid[i]);
   }
-  //std::cout << "Total Distance: " << total << std::endl;
+
+  std::cout << "Total Distance: " << l_total << std::endl;
   return 0;
 }
