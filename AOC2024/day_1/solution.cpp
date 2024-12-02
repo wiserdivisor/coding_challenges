dae7c003a372ffbdb4460fc70d635315dadc4e43
Vignesh Reddy <vreddy@tataadvancedsystems.com>
2024-12-02 17:57:52 +0530

    day 1 : added quicksort

--- AOC2024/day_1/solution.cpp
+++ AOC2024/day_1/solution.cpp
@@ -2,30 +2,47 @@
 #include <fstream>
 #include <string>
 
+int fn_partition_i(int ia_unsorted[], int i_low, int i_high) {
+  int i_pivot = ia_unsorted[i_high];
+  int i = i_low - 1;
+  for (int j = low; j < high; j++) {
+    if (ia_unsorted[j] < i_pivot) {
+      i++;
+      swap(ia_unsorted[i], ia_unsorted[j]);
+    }
+  }
+  swap(ia_unsorted[i+1], ia_unsorted[i_high]);
+  return i+1;
+}
+
+void fn_quicksort_vo(int ia_unsorted[], int i_low, int i_high) {
+  if (i_low < i_high) {
+    int i_partition_index = fn_partition_i(ia_unsorted, i_low, i_high);
+    fn_quicksort_vo(ia_unsorted, i_low, i_partition_index-1);
+    fn_quicksort_vo(ia_unsorted, i_partition_index+1, i_high);
+  }
+}
+
 int main() {
   std::cout << "Day 1 : Historian Hysteria" << std::endl;
   std::ifstream input_data("./input.txt");
-  char line[14];
-  int index = 0;
+  long total = 0;
+  char ca_line[14];
   int ia_llocid[1000];
   int ia_rlocid[1000];
-  long total = 0;
-  while(input_data.getline(line,sizeof(line))){
+  for(int i=0; i<1000; i++) {
 
-    char *ca_llocid = line;
+    input_data.getca_line(ca_line,sizeof(ca_line)); //12345\s\s\s12345
+
+    char *ca_llocid = ca_line;
     ca_llocid[5] = '\0';
-    char *ca_rlocid = line+8;
+
+    char *ca_rlocid = ca_line+8;
     ca_rlocid[14] = '\0';
 
-    int i_llocid, i_rlocid;
+    ia_llocid[i] = std::stoi(ca_llocid);
+    ia_rlocid[i] = std::stoi(ca_rlocid);
 
-    i_llocid = std::stoi(ca_llocid);
-    i_rlocid = std::stoi(ca_rlocid);
-
-    ia_llocid[index] = i_llocid;
-    ia_rlocid[index++] = i_rlocid;
-    //int diff = i_llocid - i_rlocid;
-    //total += diff;
   }
   for(auto x: ia_llocid){
     std::cout << x << std::endl;
