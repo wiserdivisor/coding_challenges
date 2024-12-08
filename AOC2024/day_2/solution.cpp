941e09a52860e2bcf8d85a69c8a21e60097cc78a
wiserdivisor <dat0user@gmail.com>
2024-12-08 06:33:35 +0530

    day 2: part 2 solved!

--- AOC2024/day_2/solution.cpp
+++ AOC2024/day_2/solution.cpp
@@ -1,59 +1,75 @@
 #include<iostream>
 #include<fstream>
 #include<string>
+#include<vector>
 
 using namespace std;
 
+bool isReportSafe(vector<int> reports_vector){
+  for(int i=1; i<(reports_vector.size()-1); i++){
+    int dist_1 = reports_vector[i-1] - reports_vector[i];
+    int dist_2 = reports_vector[i] - reports_vector[i+1];
+    if((abs(dist_1) > 3)||(abs(dist_2) > 3)) {  return false; }
+    if((dist_1 == 0)||(dist_2 == 0)) {          return false; }
+    if((dist_1 * dist_2) < 0) {                 return false; }
+  }
+  return true;
+}
+
+bool containsOnlyOneBadLevel(vector<int> reports_vector){
+  for(int i=0; i<(reports_vector.size()); i++){
+    vector<int> one_less_reports_vector = reports_vector;
+    one_less_reports_vector.erase(one_less_reports_vector.begin()+i);
+    if(isReportSafe(one_less_reports_vector)){
+      return true;
+    }
+  }
+  return false;
+}
+
+int howManyBadLevels(char *reports) {
+
+  vector<int> reports_vector;
+  char *token = strtok(reports," ");
+  while (token != NULL) {
+    int i_token = stoi(token);
+    reports_vector.push_back(i_token);
+    token = strtok(NULL," ");
+  }
+
+  if(isReportSafe(reports_vector)) {
+    return 0;
+  }
+  if(containsOnlyOneBadLevel(reports_vector)) {
+    return 1;
+  } else {
+    return 2;
+  }
+
+}
+
 int main() {
-  // readline
-  // prev_dist = 0
-  // for each two nums:
-  //   if dist > 3 or 0 or prev_dist*curr_dist is negative return
-  //   else set prev dist
-  int total_safe_no_disruptive = 0;
-  int total_safe_one_disruptive = 0;
+
+  int total_safe_no_bad = 0;
+  int total_safe_one_bad = 0;
   char line[256];
   ifstream input_data("./input.txt");
 
   for(int i=0; i<1000; i++) {
+
     input_data.getline(line,sizeof(line));
-    bool is_safe = true;
-    int i_prev_dist = 0;
-    int i_grand_prev_dist = 0;
-    int i_disruptive_nos = 0;
-    char *ca_grand_prev_num;
-    char *ca_prev_num = strtok(line," ");
-    char *ca_curr_num = strtok(NULL," ");
-    while (ca_curr_num != NULL) {      
-      int i_prev_num = stoi(ca_prev_num);
-      int i_curr_num = stoi(ca_curr_num);
-      int i_curr_dist = i_prev_num - i_curr_num;
-      if ((abs(i_curr_dist) > 3) || (i_curr_dist == 0) || ((i_prev_dist * i_curr_dist) < 0)) {
-        i_disruptive_nos++;
-        if(i_disruptive_nos == 1) {
-          ca_prev_num = ca_grand_prev_num;
-          i_prev_dist = i_grand_prev_dist;
-        } else {
-          is_safe = false;
-          break;
-        }
-      } else {
-        ca_grand_prev_num = ca_prev_num;
-        ca_prev_num = ca_curr_num;
-        i_grand_prev_dist = i_prev_dist;
-        i_prev_dist = i_curr_dist;
-        ca_curr_num = strtok(NULL," ");
-      }
+
+    int i_numBadReports = howManyBadLevels(line);
+
+    switch(i_numBadReports){
+      case 0: total_safe_no_bad++; total_safe_one_bad++; break;
+      case 1: total_safe_one_bad++; break;
+      default: break; 
     }
-    if (is_safe && i_disruptive_nos == 0) {
-      total_safe_no_disruptive++;
-      total_safe_one_disruptive++;
-    }
-    if (is_safe) {
-      total_safe_one_disruptive++;
-    }
+
   }
 
-  cout << "Total safe (no disruptive) reports: " << total_safe_no_disruptive << endl;
-  cout << "Total safe (one disruptive) reports: " << total_safe_one_disruptive << endl;
+  cout << "Total safe (no bad) reports: " << total_safe_no_bad << endl;
+  cout << "Total safe (one bad) reports: " << total_safe_one_bad << endl;
+
 }
