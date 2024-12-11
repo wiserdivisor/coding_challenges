dde0140339efa91952d61f7bf237c97912b90d8b
wiserdivisor <dat0user@gmail.com>
2024-12-11 10:53:30 +0530

    Day 3: Started.

--- AOC2024/day_3/solution.cpp
+++ AOC2024/day_3/solution.cpp
@@ -1,7 +1,19 @@
 #include<iostream>
+#include<fstream>
+#include<string>
 
 using namespace std;
 
 int main() {
-  cout << "Hello, world!" << endl;
+  
+  //char line[3500];
+  string sline;
+  ifstream input_data("./input.txt");
+
+  for(int i=0; i<6; i++) {
+    getline(input_data,sline);
+    cout << sline << endl;
+  }
+
+  input_data.close();
 }
